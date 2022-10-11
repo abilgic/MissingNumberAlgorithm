adada1ca6c7eafb28829cb6a5a6e988bc77bb39c
Ahmet Bilgiç <ahmet.bilgic@netone.net.tr>
2022-10-11 19:10:14 +0300

    Add project files.

---
+++ MissingNumberAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿        
+            int[] a = { 2, 3, 1,6, 5,7,8,9 };
+            var list = a.ToList();
+            list.Sort();
+
+            var n = a.Length;
+                       
+           for (int i = list[0], j = 0; i < n&& j < n; i++,j++)
+            {
+                if (  i != list[j] )
+                {
+                    Console.WriteLine($"{i}");
+                    break;
+                }                          
+
+            }
+
+            
+
+
+     
+    
